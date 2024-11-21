ed0a25282d406853666dbfae39aeef0152041e38
Theo Lee <theo.leehao@elev.ga.ntig.se>
2024-11-21 15:17:10 +0100

    finish

--- Steg 2/Program.cs
+++ Steg 2/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+using System.Security.Authentication;
+
+// List<string> Items = ["Ball", "Lego", "Doll", "Action Figure", "Stuffed Animal"];
+
+// foreach (string Item in Items)
+// {
+//     Console.WriteLine(Item);
+// }
+
+// List<string> Names = ["Phatchara", "Teodora", "Liam", "Andreas", "Mande"];
+
+// List<int> Numbers = [2, 4, 5, 7, 9, ];
+
+// for (int i = 0; i < Names.Count; i++)
+// {
+//     Console.WriteLine($"{Names[i]} får en {Items[i]} för {Numbers[i]} kr");
+// }
+
+
+List<string> cities = new List<string>();
+string input;
+
+while (true)
+{
+    System.Console.WriteLine("Enter the name of a city");
+    input = Console.ReadLine();
+    while (string.IsNullOrEmpty(input))
+    {
+        System.Console.WriteLine("Please enter the name of a city");
+        input = Console.ReadLine();
+    }
+
+    cities.Add(input);
+
+    if (input == "exit")
+    {
+        break;
+    }
+
+    for (int i = 0; i < cities.Count; i++)
+    {
+        System.Console.WriteLine(cities[i]);
+    }
+
+}
+
+
+Console.ReadLine();
